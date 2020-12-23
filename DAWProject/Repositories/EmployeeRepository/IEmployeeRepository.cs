92dc9861e4dd28c0a1a2ba7649a6a38572df1537
aalinaaw <popa.alina98@yahoo.com>
2020-12-24 01:41:35 +0200

    missing repositories

---
+++ DAWProject/Repositories/EmployeeRepository/IEmployeeRepository.cs
@@ -0,0 +1,10 @@
+using DAWProject.Models;
+using DAWProject.Repositories.GenericRepository;
+
+namespace DAWProject.Repositories.EmployeeRepository
+{
+    public interface IEmployeeRepository : IGenericRepository<Employee>
+    {
+        
+    }
+}
